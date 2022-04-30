5a9045f0125deb7c4a9ad5e70ea3b5a344307e7c
Will-2332 <willian.musquim@gmail.com>
2022-04-30 20:26:12 +0100

    Found the routes I did before and worked.

--- server.js
+++ server.js
@@ -219,16 +219,7 @@ app.post('/login',
         failureMessage: true
     }),
 );
-app.delete("/coins/:id", async (request, response) => {
-    try {
-      const coins = await coinsModel.findByIdAndDelete(request.params.id);
 
-      if (!coins) response.status(404).send("No item found");
-      response.status(200).send();
-    } catch (error) {
-      response.status(500).send(error);
-    }
-  });
 
 app.get('/managment', loggedIn, async (req, res,next) => {
     const data = await coinHandler.findCoins();
@@ -237,6 +228,56 @@ app.get('/managment', loggedIn, async (req, res,next) => {
         res.send(str)
     });
 });
+
+app.get("/coins", async (request, response) => {
+    const coins = await coinsModel.find({});
+  
+    try {
+      response.send(coins);
+    } catch (error) {
+      response.status(500).send(error);
+    }
+  });
+  
+  app.post("/coins", async (request, response) => {
+      const coins = new coinsModel(request.body);
+    
+      try {
+        await coins.save();
+        response.send(coins);
+      } catch (error) {
+        response.status(500).send(error);
+      }
+    });
+  
+    app.patch("/coins/:id", async (request, response) => {
+      try {
+        await coinsModel.findByIdAndUpdate(request.params.id, request.body);
+        await coinsModel.save();
+        response.send(coin);
+      } catch (error) {
+        response.status(500).send(error);
+      }
+    });
+  
+    app.delete("/coins/:id", async (request, response) => {
+      try {
+        const coins = await coinsModel.findByIdAndDelete(request.params.id);
+    
+        if (!coins) response.status(404).send("No item found");
+        response.status(200).send();
+      } catch (error) {
+        response.status(500).send(error);
+      }
+    });
+  
+    app.get('/coins/:id', (req, res) => {
+      Coins.find({}, function(err, coins) {
+          res.render('index.html', {
+              CoinsList: coins
+          })
+      })
+  })
 
 app.get('/register', async (req, res) => {
 
